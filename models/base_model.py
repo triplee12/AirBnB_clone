2ac745279daa94f7d77c06ce26dcc8ae493b8a01
Dolapo <Tichiegoju@gmail.com>
2023-02-07 04:36:11 -0800

    Done

---
+++ models/base_model.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Defines the BaseModel class."""
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """Represents the BaseModel of the HBnB project."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel.
+
+        Args:
+            *args: list of arguments
+            **kwargs: dict of key-values arguments
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Returns official string representation of the basemodel"""
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+
+    def save(self):
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__"""
+        my_dict = self.__dict__.copy()
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        my_dict["__class__"] = self.__class__.__name__
+        return my_dict
