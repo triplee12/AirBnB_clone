28aaf7ba603e67142b4d4bc3172f5c5520559d17
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2023-02-08 11:58:38 -0800

    Create User class

---
+++ models/user.py
@@ -0,0 +1,14 @@
+"""
+User module
+"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """
+    User class inherits from BaseModel
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
